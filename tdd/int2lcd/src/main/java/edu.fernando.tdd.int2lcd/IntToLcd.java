94fa6728a1ac7622b5ee3b5e07d8ee8f91aa93dc
Fernando Luna <337n4nd0@gmail.com>
2018-06-15 04:50:52 -0400

    pass test ConvertLargeIntegerToArrayOfStrings

--- tdd/int2lcd/src/main/java/edu.fernando.tdd.int2lcd/IntToLcd.java
+++ tdd/int2lcd/src/main/java/edu.fernando.tdd.int2lcd/IntToLcd.java
@@ -3,6 +3,17 @@ package edu.fernando.tdd.int2lcd;
 public class IntToLcd {
 
     public String[] convertIntegerToArrayOfStrings(int number) {
+        if (number > 9) {
+            int[] individualDigits = convertIntegerToArrayOfIndividualDigits(number);
+            String[] result = new String[] {"","",""};
+            for (int singleNumber: individualDigits) {
+                String[] arrayStringDigit = convertIntegerToArrayOfStrings(singleNumber);
+                result = mergeTwoArraysOfString(result,arrayStringDigit);
+            }
+            result = mergeTwoArraysOfString(result,new String[] {"","",""});
+            return result;
+
+        }
         switch (number) {
             case 1:
                 return new String[] {" ","|","|"};
