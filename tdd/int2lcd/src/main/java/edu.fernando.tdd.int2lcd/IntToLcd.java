9d9dd0ee268e72cb97de90f9ea78d15dfea77a90
Fernando Luna <337n4nd0@gmail.com>
2018-06-15 03:36:32 -0400

    pass test GivenIntegerOneToArrayOfStrings

--- tdd/int2lcd/src/main/java/edu.fernando.tdd.int2lcd/IntToLcd.java
+++ tdd/int2lcd/src/main/java/edu.fernando.tdd.int2lcd/IntToLcd.java
@@ -2,4 +2,10 @@ package edu.fernando.tdd.int2lcd;
 
 public class IntToLcd {
 
+    public String[] convertIntegerToArrayOfStrings(int number) {
+        if (number == 1) {
+            return new String[] {"","|","|"};
+        }
+        return new String[0];
+    }
 }
