8b2fa261049c9fe30e5e471fc66b21bfaec2ce0c
Fernando Luna <337n4nd0@gmail.com>
2018-06-15 04:56:56 -0400

    add use application in main

--- tdd/int2lcd/src/main/java/edu.fernando.tdd.int2lcd/Main.java
+++ tdd/int2lcd/src/main/java/edu.fernando.tdd.int2lcd/Main.java
@@ -1,9 +1,16 @@
 package edu.fernando.tdd.int2lcd;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        IntToLcd int2lcd = new IntToLcd();
+        int randomNum = ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE);
+        String[] numberArrayString = int2lcd.convertIntegerToArrayOfStrings(randomNum);
+        for (String text: numberArrayString) {
+            System.out.println(text);
+        }
     }
 
 }
