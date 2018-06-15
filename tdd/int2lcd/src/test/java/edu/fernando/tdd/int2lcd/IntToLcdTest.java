cddad88264ca52d3d06a22ba2c5b7cef7c85a054
Fernando Luna <337n4nd0@gmail.com>
2018-06-15 03:32:40 -0400

    add test GivenIntegerOneToArrayOfStrings

--- tdd/int2lcd/src/test/java/edu/fernando/tdd/int2lcd/IntToLcdTest.java
+++ tdd/int2lcd/src/test/java/edu/fernando/tdd/int2lcd/IntToLcdTest.java
@@ -1,5 +1,16 @@
 package edu.fernando.tdd.int2lcd;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class IntToLcdTest {
+
+    @Test
+    public void testGivenIntegerOneToArrayOfStrings() {
+        IntToLcd intToLcs = new IntToLcd();
+        String[] arrayStringOne = intToLcs.convertIntegerToArrayOfStrings(1);
+        String[] expected = new String[] {"","|","|"};
+        Assert.assertArrayEquals(arrayStringOne, expected);
+    }
 
 }
